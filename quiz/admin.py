879584e28fa9071bad5560b24d57d390807d5ef0
amangaliev ersyn <amangalieversyn@gmail.com>
2025-05-16 13:02:31 +0500

    Create models and regiter this model ro admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Question, Session, UserAnswer
 
 # Register your models here.
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'text', 'correct_option')
+    search_fields = ('text',)
+    list_filter = ('user',)
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'created_at', 'mode', 'status')
+    list_filter = ('status', 'mode', 'created_at')
+    search_fields = ('user__username',)
+
+@admin.register(UserAnswer)
+class UserAnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'session', 'question', 'selected_option', 'is_correct')
+    list_filter = ('is_correct',)
+    search_fields = ('user__username', 'question__text')
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,68 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Question(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    option_a = models.CharField(max_length=255)
+    option_b = models.CharField(max_length=255)
+    option_c = models.CharField(max_length=255)
+    option_d = models.CharField(max_length=255)
+
+    CHOICES = [
+        ('A', 'Variant A'),
+        ('B', 'Variant B'),
+        ('C', 'Variant C'),
+        ('D', 'Variant D'),
+    ]
+    correct_option = models.CharField(max_length=1, choices=CHOICES)
+
+    def __str__(self):
+        return self.text
+
+
+class Session(models.Model):
+    MODE_CHOICES = [
+        ('O', 'Order'),
+        ('R', 'Random'),
+    ]
+    STATUS_CHOICES = [
+        ('O', 'Opened'),
+        ('C', 'Closed'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    mode = models.CharField(max_length=1, choices=MODE_CHOICES)
+    status = models.CharField(
+        max_length=1,
+        choices=STATUS_CHOICES,
+        default='O')
+
+    def __str__(self):
+        return f"Session {self.pk} — {self.user.username}"
+
+# Ответ пользователя в рамках конкретной сессии
+
+
+class UserAnswer(models.Model):
+    CHOICES = [
+        ('A', 'Variant A'),
+        ('B', 'Variant B'),
+        ('C', 'Variant C'),
+        ('D', 'Variant D'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    selected_option = models.CharField(max_length=1, choices=CHOICES)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{
+            self.user.username} answered {
+            self.selected_option} on {
+            self.question.id}"
