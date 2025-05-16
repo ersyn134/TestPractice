9053b5e0288380fa6a4032ac4bcf61faabaeb4d8
amangaliev ersyn <amangalieversyn@gmail.com>
2025-05-16 12:39:57 +0500

    Registration login logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
-# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # автоматический вход после регистрации
+            return redirect('home')  # страница после входа
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def home_view(request):
+    return render(request, 'home.html')
+
+@login_required
+def profile_view(request):
+    return render(request, 'profile.html')
