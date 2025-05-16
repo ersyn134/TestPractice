6dd7fdcc0abf1881caca3b9a2626aab4f3196787
amangaliev ersyn <amangalieversyn@gmail.com>
2025-05-16 17:34:42 +0500

    Просмотр, редактирование и удаление вопросов

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import add_question
+from .views import add_question,list_questions,edit_question,delete_question
 urlpatterns = [
     path('add_question/', add_question, name='question_add'),
+    path('list_questions/', list_questions, name='questions_list'),
+    path('edit_question/<int:id>', edit_question, name='edit_question'),
+    path('delete_question/<int:id>', delete_question, name='delete_question'),
+
+
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import QuestionForm
+from .models import Question
+
 
 @login_required
 def add_question(request):
@@ -14,3 +16,27 @@ def add_question(request):
     else:
         form = QuestionForm()
     return render(request, 'add_question.html', {'form': form})
+
+def list_questions(request):
+    questions = Question.objects.all().filter(user=request.user)
+    return render(request,'questions_list.html', {'questions': questions})
+
+def edit_question(request, id):
+    question = get_object_or_404(Question, id=id, user=request.user)
+    if request.method == 'POST':
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = QuestionForm(instance=question)
+
+    return render(request, 'edit_question.html', {'form': form})
+
+def delete_question(request, id):
+    question = get_object_or_404(Question, id=id, user=request.user)
+    if request.method == 'POST':
+        question.delete()
+        return redirect('home')
+
+    return render(request, 'delete_question.html', {'question': question})
