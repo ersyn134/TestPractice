b3d84379271ed1eba82e0d35241985902f9b03d4
amangaliev ersyn <amangalieversyn@gmail.com>
2025-05-19 13:29:21 +0500

    Realize variants

--- apps/quiz/admin.py
+++ apps/quiz/admin.py
@@ -3,11 +3,7 @@ from .models import Question, Session, UserAnswer,SessionQuestion
 
 # Register your models here.
 
-@admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'text', 'correct_option', 'created_at')
-    search_fields = ('text',)
-    list_filter = ('user',)
+
 
 @admin.register(Session)
 class SessionAdmin(admin.ModelAdmin):
@@ -15,11 +11,27 @@ class SessionAdmin(admin.ModelAdmin):
     list_filter = ('status', 'mode', 'created_at')
     search_fields = ('user__username',)
 
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'user', 'text',
+        # заменяем correct_option на correct_text
+        'correct_text',
+        'created_at'
+    )
+    search_fields = ('text',)
+    list_filter = ('user',)
+
 @admin.register(UserAnswer)
 class UserAnswerAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'session', 'question', 'selected_option', 'is_correct')
+    list_display = (
+        'id', 'user', 'session', 'question',
+        # заменяем selected_option на selected_text
+        'selected_text',
+        'is_correct'
+    )
     list_filter = ('is_correct',)
-    search_fields = ('user__username', 'question__text')
+    search_fields = ('user__username','question__text')
 
 @admin.register(SessionQuestion)
 class SessionQuestionAdmin(admin.ModelAdmin):
